610e8300866ff8b695459f504c4ac346150fd5c5
LordofGhost <134922046+LordofGhost@users.noreply.github.com>
2024-04-06 14:24:09 +0200

    select canvas size

--- script.js
+++ script.js
@@ -1,13 +1,24 @@
 const debugMode = true;
-const container = document.querySelector("#container");
-let rows = 16;
-let columns = 16;
+const container = document.getElementById('container');
+const inputWidth = document.getElementById('width');
+const inputHeight = document.getElementById('height');
+const submitSize = document.getElementById('submitSize');
 
-function init () {
+submitSize.addEventListener('click', () => {
+    let rows = inputWidth.value;
+    let columns = inputWidth.value;
+    if (rows > 0 && rows <= 100 && columns > 0 && columns <= 100) init(rows, columns); else prompt("max 100x100");
+});
+
+function init (rows, columns) {  // creates the canvas
+    while (container.lastElementChild) { // delets all old Elements
+        container.removeChild(container.lastElementChild);
+    }
     for (let i = 0; i < rows; i++) {
         // creates one new row
         const row = document.createElement("div");
         row.classList.add("row");
+        container.appendChild(row);
     
         if (debugMode) console.log("Created new row");
     
@@ -15,6 +26,9 @@ function init () {
             // creates a square and puts it inside row
             const square = document.createElement("div");
             square.classList.add("square");
+            square.addEventListener('mouseover', () => {
+                square.style.backgroundColor = "black";
+            });
             row.appendChild(square)
     
             if (debugMode) console.log("Created new square");
@@ -23,4 +37,4 @@ function init () {
     if (debugMode) console.log("Finished creating canvas");
 }
 
-init();
+init(16, 16);
