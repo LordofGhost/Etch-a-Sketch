16cc784a860228aeb6960c12b264c446fa61f58f
LordofGhost <134922046+LordofGhost@users.noreply.github.com>
2024-05-02 18:27:07 +0200

    improve styling

--- script.js
+++ script.js
@@ -1,26 +1,31 @@
-const debugMode = true;
 const container = document.getElementById('container');
 const size = document.getElementById('size');
 const submitSize = document.getElementById('submitSize');
-const randomColor = document.getElementById('random');
 const colorSelectors = document.querySelectorAll('.color')
 let color = 'black';
+let isMouseDown = false;
+
+let mouseDown = false
+document.body.onmousedown = () => (mouseDown = true)
+document.body.onmouseup = () => (mouseDown = false)
 
 submitSize.addEventListener('click', () => {
     let rows = size.value;
     let columns = size.value;
-    if (rows > 0 && rows <= 100 && columns > 0 && columns <= 100) init(rows, columns); else if (debugMode) console.error("Max size 100");
+    // checks if it si valid input 
+    if (rows > 0 && rows <= 100 && columns > 0 && columns <= 100) init(rows, columns); else alert("Max 100");
 });
 
 colorSelectors.forEach(colorSelector => {
     colorSelector.addEventListener('click', () => {
-        if (colorSelector.textContent == 'random') { 
-            color = 'random'
-        } else {
-            color = colorSelector.style.backgroundColor;
-        }
+        color = colorSelector.style.backgroundColor;
     }); 
 });
+
+function changeColor (e) {
+    if (e.type === 'mouseover' && !mouseDown) return
+    e.target.style.backgroundColor = color;
+}
 
 function init (rows, columns) {  // creates the canvas
     while (container.lastElementChild) { // delets all old Elements
@@ -32,27 +37,17 @@ function init (rows, columns) {  // creates the canvas
         row.classList.add("row");
         container.appendChild(row);
     
-        if (debugMode) console.log("Created new row");
-    
         for (let j = 0; j < columns; j++) {
             // creates a square and puts it inside row
             const square = document.createElement("div");
             square.style.backgroundColor = 'white';
             square.classList.add("square");
-            square.addEventListener('click', () => {
-                if (color == 'random') {
-                    // creating a new color for each element
-                    square.style.backgroundColor = '#' + Math.floor(Math.random()*16777215).toString(16);
-                } else {
-                    square.style.backgroundColor = color;
-                }
-            });
+
+            square.addEventListener('mouseover', changeColor);
+            square.addEventListener('mousedown', changeColor);
             row.appendChild(square)
-    
-            if (debugMode) console.log("Created new square");
         }
     }
-    if (debugMode) console.log("Finished creating canvas");
 }
 
 init(16, 16);
